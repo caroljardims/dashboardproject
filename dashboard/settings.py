2d928851b8c697616a328ad7b8e3b290871c6212
caroljardims <caroljardims@gmail.com>
2016-01-19 16:38:33 -0200

    add csvimport steps

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #https://github.com/edcrewe/django-csvimport
     'csvimport.app.CSVImportConf',
     'data',
 )
