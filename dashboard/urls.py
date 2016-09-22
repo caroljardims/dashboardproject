b9667a8c7a3b6a653b95505fdcfb0d8c9e4447c0
Caroline <caroljardims@gmail.com>
2016-09-22 15:10:33 -0300

    alterar url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^dashboard/admin/', include(admin.site.urls)),
     url(r'^$', 'data.views.index', name='index'),
-    url(r'^index', 'data.views.index', name='index'),
-    url(r'^centros', 'data.views.centros', name='centros'),
-    url(r'^centro/([0-9]*)/$', 'data.views.centro', name='centro'),
-    url(r'^curso/([0-9]*)/$', 'data.views.curso', name='curso'),
-    url(r'^general', 'data.views.general', name='Geral UFSM'),
-    url(r'^source/([0-9]*)/$', 'data.views.origemAlunos', name='origem_alunos'),
+    url(r'^$dashboard/', 'data.views.index', name='index'),
+    url(r'^dashboard/index', 'data.views.index', name='index'),
+    url(r'^dashboard/centros', 'data.views.centros', name='centros'),
+    url(r'^dashboard/centro/([0-9]*)/$', 'data.views.centro', name='centro'),
+    url(r'^dashboard/curso/([0-9]*)/$', 'data.views.curso', name='curso'),
+    url(r'^dashboard/general', 'data.views.general', name='Geral UFSM'),
+    url(r'^dashboard/source/([0-9]*)/$', 'data.views.origemAlunos', name='origem_alunos'),
 ]
